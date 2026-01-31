70879e8cf9861154af34ee7deb045ef0373a7f94
Kaktys868 <lastochka1230@yandex.ru>
2026-01-31 12:38:26 +0500

    Разработанное главное окно. Тошматов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PrintManagementSystem_Toshmatov
+namespace Print_Management_System
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // 1. Удаление операции
+        private void DeleteOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // 2. Редактирование операции
+        private void EditOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // 3. Изменение вида работы
+        private void SelectedType(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        // 4. Изменение формата бумаги
+        private void SelectedFormat(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        // 5. Изменение количества
+        private void textBoxCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Обработчик есть, но пустой
+        }
+
+        // 6. Проверка ввода (только цифры)
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Разрешаем только цифры
+            
+        }
+
+        // 7. Добавление операции
+        private void AddOperation(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 8. Изменение параметров цвета/сторон
+        private void ColorsChange(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
