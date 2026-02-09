06039443049eab812cc4247166fe1ddc39ee8700
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-02-09 11:17:27 +0500

    Реализованные классы. Тошматов..

--- Classes/JournalRecord.cs
+++ Classes/JournalRecord.cs
@@ -4,15 +4,15 @@ namespace Print_Management_System.Classes
 {
     public class JournalRecord
     {
-        public string User { get; set; }               // Фамилия и инициалы
-        public string OperationType { get; set; }      // Вид работы (Печать, Копия и т.д.)
-        public string Format { get; set; }             // Формат (A4, A3, A2, A1)
-        public int Side { get; set; }                  // Сторона (1 или 2)
-        public string Color { get; set; }              // Цвет (Ч/Б или ЦВ)
-        public int Count { get; set; }                 // Количество
-        public decimal Price { get; set; }             // Сумма
-        public string Signature { get; set; }          // Подпись
-        public DateTime Date { get; set; }             // Дата операции
+        public string User { get; set; }               
+        public string OperationType { get; set; }    
+        public string Format { get; set; }        
+        public int Side { get; set; }                  
+        public string Color { get; set; }              
+        public int Count { get; set; }                 
+        public decimal Price { get; set; }             
+        public string Signature { get; set; }         
+        public DateTime Date { get; set; }           
 
         public JournalRecord()
         {
