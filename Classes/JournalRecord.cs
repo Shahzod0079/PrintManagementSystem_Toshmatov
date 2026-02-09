b4258a91d05792d9467edf941b8948a952d07c95
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-02-09 11:13:53 +0500

    Разработанное окно Журнал операций н оценку отлично. Тошматов.

---
+++ Classes/JournalRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Print_Management_System.Classes
+{
+    public class JournalRecord
+    {
+        public string User { get; set; }               // Фамилия и инициалы
+        public string OperationType { get; set; }      // Вид работы (Печать, Копия и т.д.)
+        public string Format { get; set; }             // Формат (A4, A3, A2, A1)
+        public int Side { get; set; }                  // Сторона (1 или 2)
+        public string Color { get; set; }              // Цвет (Ч/Б или ЦВ)
+        public int Count { get; set; }                 // Количество
+        public decimal Price { get; set; }             // Сумма
+        public string Signature { get; set; }          // Подпись
+        public DateTime Date { get; set; }             // Дата операции
+
+        public JournalRecord()
+        {
+            Date = DateTime.Now;
+            Signature = "Подпись";
+        }
+
+        // Конструктор для создания из TypeOperationsWindow
+        public JournalRecord(TypeOperationsWindow operation, string user)
+        {
+            User = user;
+            OperationType = operation.typeOperationText;
+            Format = operation.formatText;
+            Side = operation.side;
+            Color = operation.colorText;
+            Count = operation.count;
+            Price = (decimal)operation.price;
+            Signature = "Подпись";
+            Date = DateTime.Now;
+        }
+    }
+}
--- Classes/TypeOperationsWindow.cs
+++ Classes/TypeOperationsWindow.cs
@@ -1,27 +1,33 @@
-﻿namespace Print_Management_System.Classes
-{
+﻿using System;
 
+namespace Print_Management_System.Classes
+{
     public class TypeOperationsWindow
     {
-
         public string typeOperationText { get; set; }
-
         public string formatText { get; set; }
-
         public string colorText { get; set; }
-
         public int typeOperation { get; set; }
-
         public int format { get; set; }
-
         public int side { get; set; }
-
         public bool color { get; set; }
-
         public bool occupancy { get; set; }
-
         public int count { get; set; }
-
         public float price { get; set; }
+
+        public JournalRecord ToJournalRecord(string user)
+        {
+            return new JournalRecord
+            {
+                User = user,
+                OperationType = typeOperationText,
+                Format = formatText,
+                Side = side,
+                Color = colorText,
+                Count = count,
+                Price = (decimal)price,
+                Date = DateTime.Now
+            };
+        }
     }
 }
